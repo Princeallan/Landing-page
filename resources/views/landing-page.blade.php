99332250cc149ee4473f938adfbd829a87e5ca03
princeallan <allankirui57@gmail.com>
2019-01-07 22:35:47 +0300

    home slider added

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -3,34 +3,29 @@
 @section('content')
 
 
-              <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
+<div class="carousel slide" data-ride="carousel">
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <img src="{{ asset('images/pynade1.jpg')}}" alt="" style="width:100%;">
-            </div>
-
-            <div class="item">
-                <img src="{{ asset('images/pynade2.jpg')}}" alt="" style="width:100%;">
-            </div>
-
-            <div class="item">
-                <img src="{{ asset('images/pynade1.jpg')}}" alt="" style="width:100%;">
-            </div>
-        </div>
-
-        <!-- Left and right controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-        </a>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ asset('images/pynade1.jpg') }}" class="d-block w-100" alt="...">
     </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/pynade1.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('images/pynade1.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
 
 
 @endsection
